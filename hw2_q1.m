9fe22a25aa8cdbd76e3f6f31ed5f89788e259644
jhennin <jhennin@users.noreply.github.com>
2016-04-29 14:33:52 -0400

    Added files via upload

---
+++ hw2_q1.m
@@ -0,0 +1,18 @@
+% Explain majority carrier and minority carrier in n-type semiconductor
+% materials:
+
+% P-type and N-tpe materials have majority and minority carriers
+
+% Majority carriers have more of a certain property, whether it is holes or
+% electrons.
+
+% Minority carriers have less of a certain property, whether it is holes or
+% electrons.
+
+% P-Type material has ecess holes and very few electrons. It can be said
+% that for P-type materials holes are the majority carrier and electrons
+% are the minority carrier.
+
+% N-type material has excess electrons and few holes. It can be said for
+% N-type materials that they electrons as the majority carrier and holes as
+% the minority carrier
